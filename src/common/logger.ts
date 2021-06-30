8843a5ff88e784224e414579aa7a2fdf7af76f93
xsahxl <xsahxl@126.com>
2021-06-30 16:57:53 +0800

    feat: 🎸 logger 更改使用方式

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,35 +1,5 @@
 import { Logger } from '@serverless-devs/core';
 
-export default class ComponentLogger {
-  static CONTENT = 'jamstack-api';
-  static setContent(content) {
-    ComponentLogger.CONTENT = content;
-  }
+const logger = new Logger('jamstack-api');
 
-  static log(m) {
-    Logger.log(m);
-  }
-
-  static info(m) {
-    Logger.info(ComponentLogger.CONTENT, m);
-  }
-
-  static debug(m) {
-    Logger.debug(ComponentLogger.CONTENT, m);
-  }
-
-  static error(m) {
-    Logger.error(ComponentLogger.CONTENT, m);
-  }
-
-  static warning(m) {
-    Logger.warn(ComponentLogger.CONTENT, m);
-  }
-
-  static success(m) {
-    Logger.log(m, 'green');
-  }
-}
-
-
-
+export default logger;
