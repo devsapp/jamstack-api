ea9169760d08a2b37e41d246f124b1f31377e919
xsahxl <xsahxl@126.com>
2021-08-24 11:19:00 +0800

    feat: 🎸 add ncc

--- lib/common/base.d.ts
+++ lib/common/base.d.ts
@@ -1,12 +1,3 @@
 export default class BaseComponent {
-    protected inputs: any;
-    protected client: any;
-    name: string;
-    private basePath;
-    constructor(inputs: any);
-    __getBasePath(): string;
-    private getEntityByName;
-    private getEntityHelpInfoByName;
-    protected help(name: any): void;
     protected __report(reportData: ServerlessDevsReport.ReportData): ServerlessDevsReport.JamstackApi[];
 }
--- lib/lib/cdn/EdgeScript.d.ts
+++
@@ -1,3 +0,0 @@
-export default class addEdgeScript {
-    constructor(accessKeyId: string, accessKeySecret: string);
-}
